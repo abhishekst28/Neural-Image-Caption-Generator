e42e99a7e170f0e51ad505d1601a5a0b785e7524
abhishekst28 <55763935+abhishekst28@users.noreply.github.com>
2020-05-03 19:09:11 +0530

    text to speech added

--- static/js/main.js
+++ static/js/main.js
@@ -16,6 +16,18 @@ $(document).ready(function () {
             reader.readAsDataURL(input.files[0]);
         }
     }
+
+    function playSound(url){
+  var audio = document.createElement('audio');
+  audio.style.display = "none";
+  audio.src = url;
+  audio.autoplay = true;
+  audio.onended = function(){
+    audio.remove() //Remove when played.
+  };
+  document.body.appendChild(audio);
+}
+
     $("#imageUpload").change(function () {
         $('.image-section').show();
         $('#btn-predict').show();
@@ -45,7 +57,26 @@ $(document).ready(function () {
                 // Get and display the result
                 $('.loader').hide();
                 $('#result').fadeIn(600);
-                $('#result').text(' Result:  ' + data);
+                $('#result').text(' Result : ' + data);
+                var k=data.search("delimits");
+                var text_style=data.substring(0,k);
+                var audio_style=data.substring(k+9);
+                //document.getElementById("demo1").innerHTML = text_style;
+                //document.getElementById("demo2").innerHTML = audio_style;
+                console.log(text_style);
+                console.log(audio_style);
+                $('#result').text(' Result :  ' + text_style);
+                //var music= new Audio(audio_style)
+                //music.play();
+                //console.log("once");
+
+                //var music2= new Audio("C:/Users/toshn/Desktop/mits/example.mp3")
+                //music2.play();
+                //console.log("tonce");
+                //playSound(C:/Users/toshn/Desktop/mits/example.mp3);
+                //playSound("C:/Users/toshn/Desktop/mits/example.mp3");
+                //console.log("3once");
+
                 console.log('Success!');
             },
         });
